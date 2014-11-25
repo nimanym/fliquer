f7e6184c69dd920bad4ab4893679b777c2c21e46
Venisir <sithis77@gmail.com>
2014-11-25 22:38:08 +0100

    Con la lista de paises

--- nuevoUsuario.php
+++ nuevoUsuario.php
@@ -7,6 +7,28 @@ require_once("cabecera.inc");
 
 ?>
 
+<?php
+// Conecta con el servidor de MySQL
+$link = @mysqli_connect(
+'localhost', // El servidor
+'root', // El usuario
+'', // La contraseña
+'pibd'); // La base de datos
+if(!$link) {
+	echo '<p>Error al conectar con la base de datos: ' . mysqli_connect_error();
+	echo '</p>';
+	exit;
+}
+
+$sentencia = 'SELECT NomPais FROM paises';
+if(!($resultado = @mysqli_query($link, $sentencia))) {
+echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . mysqli_error($link);
+echo '</p>';
+exit;
+}
+
+?>
+
 <form action="register.php" method="post" id="formularioRegistro">
 	<h2>Registrarse</h2>
 	Nombre de usuario: <br><input required type="text" name="nombreUsuario" id="nombreUsuario"><br>
@@ -14,6 +36,22 @@ require_once("cabecera.inc");
 	Verificar contraseña: <br><input required type="password" name="password2" id="password2"><br>
 	E-mail: <br><input required type="email" name="email" id="email"><br>
 	Fecha de nacimiento: <br><input required type="date" name="fechaNacimiento" id="fechaNacimiento"><br>
+	Pais: <br>
+
+
+	<select name="pais">  
+
+       <option value="" selected="selected"></option>
+		<?php
+		while($nomPaises = mysqli_fetch_assoc($resultado)) {
+			echo '<option value="' . $nomPaises['NomPais'] . '">' . $nomPaises['NomPais'] . '</option>';
+		}
+		?>
+    </select>
+
+    <br>
+
+
 	Sexo: <br />
 	<input type="radio" checked name="sexo" value="hombre" id="sexoHombre">Hombre<br>
 	<input type="radio" name="sexo" value="mujer" id="sexoMujer">Mujer<br>
