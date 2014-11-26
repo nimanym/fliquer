c34e11fbbcf62efc3370aa4363c94b73429b1f60
nimanym <nimanym@gmail.com>
2014-11-26 23:11:06 +0100

    Crearalbum y misalbumes

---
+++ misalbumes.php
@@ -0,0 +1,46 @@
+<?php
+// Título de la página, se muestra en <title> y en el cuerpo de la página con <h2>
+$title = "Mis álbumes - Fliquer";
+$nombre = "Mis álbumes";
+
+require_once("cabecera.inc");
+
+if(!isset($_SESSION['nombreUsu'])){
+	header('Location: nuevoUsuario.php');
+}
+
+?>
+
+<script src="cambioEstilos.js"></script>
+<script src="validacion.js"></script>
+
+
+<?php
+// Conecta con el servidor de MySQL
+$link = @mysqli_connect(
+'localhost', // El servidor
+'root', // El usuario
+'', // La contraseña
+'pibd'); // La base de datos
+if(!$link) {
+	echo '<p>Error al conectar con la base de datos: ' . mysqli_connect_error();
+	echo '</p>';
+	exit;
+}
+
+$sentencia = 'SELECT a.* FROM albumes a, usuarios u where a.Usuario=u.IdUsuario and u.NomUsuario=$_SESSION['nombreUsu']';
+if(!($resultado = @mysqli_query($link, $sentencia))) {
+echo "<p>Error al ejecutar la sentencia <b>$sentencia</b>: " . mysqli_error($link);
+echo '</p>';
+exit;
+}
+?>
+
+while($fila = mysqli_fetch_assoc($resultado)) {
+echo '<p>' . $fila['Titulo'] . '</p>';
+}
+
+
+<?php
+require_once("footer.inc");
+?>
